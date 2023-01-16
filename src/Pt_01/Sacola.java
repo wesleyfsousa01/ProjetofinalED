2bb18922264b321dbf03bb4b325d43c16128295e
wesleyfsousa01 <wesleyfsouza10@gmail.com>
2023-01-16 08:51:24 -0300

    Implementação metodos de ordenação Implementação Sacola Atualizações gerais

--- src/Pt_01/Sacola.java
+++ src/Pt_01/Sacola.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import Metodos_Ordenacao.Metodos;
 
 /**
  *
@@ -19,8 +20,8 @@ import java.util.List;
 public class Sacola {
     
     public static void main (String [] args){
-        List<Integer> numerosRemovidos = new ArrayList();
-        List numerosIseridos = new ArrayList();
+        ArrayList numerosRemovidos = new ArrayList();
+        ArrayList numerosIseridos = new ArrayList();
         Scanner sc = new Scanner(System.in);
         
         int numeroDeOperacoes;
@@ -32,6 +33,7 @@ public class Sacola {
                 
                 boolean tipoPilha = true;
                 boolean tipoFila = true;
+                boolean tipoFilaDePrioridade = true;
                 for (int i=0;i<numeroDeOperacoes;i++){
                     
                     System.out.println("Digite a operação que deseja realizar");
@@ -40,12 +42,12 @@ public class Sacola {
                     if(operacao == 1){
                         System.out.println("Digite o número que deseja inserir na sacola");
                         int numero = sc.nextInt();
-                        if(numero >= 1 && numero <= 100){
+                        if(numero >= 0 && numero <= 100){
                             lista.add(numero);
                             numerosIseridos.add(numero);
                             
                         }else {
-                            System.out.println("Digite um número entre 0 e 100!!");
+                            System.out.println("Digite um número entre 0 e 100");
                                 numeroDeOperacoes +=1; 
                             }
                     } else if (operacao == 2){
@@ -58,15 +60,32 @@ public class Sacola {
                                 try {
                                     int n = sc.nextInt();
                                     
-                                    //numerosRemovidos.add(n); //Salva as remoções;
-                                    if(!lista.get(lista.size()-1).equals(n)){
-                                        tipoPilha = false;
-                                    }else if(!lista.get(0).equals(n)){
-                                        tipoFila = false;
-                                    }
-                                    lista.remove(Integer.valueOf(n)); //Remove o elemento espeficado da lista 
-                                    
+                                    if(lista.contains(n)){
+                                        
+                                        if(!lista.get(lista.size()-1).equals(n)){
+                                            tipoPilha = false;
+                                        }else if(!lista.get(0).equals(n)){
+                                            tipoFila = false;
+                                        
+                                        }else if(n >(int) numerosRemovidos.get(0)){
+                                            tipoFilaDePrioridade = false;
+                                        }
+                                    numerosRemovidos.add(n); //Salva as remoções;
+                                        
+                                    lista.remove(Integer.valueOf(n)); 
+                                    //Remove o elemento espeficado da lista 
                                     System.out.println("Numero removido: " + n);
+                                    //números da sacola
+                                    System.out.println("Sacola:");
+                                    System.out.println(lista.toString());
+                                    } else{
+                                            System.out.println("O número não existe na Sacola");
+                                            numeroDeOperacoes +=1;
+                                            
+                                        }                            
+
+                                    
+                     
                                     
                                 } catch (Exception e) {
                                     System.out.println("O número informado não existe na sacola");
@@ -74,20 +93,24 @@ public class Sacola {
                                 }
                             }
                         }else{
-                            System.out.println("Só são válidas duas opções!! \n)");
+                            System.out.println("Só são válidas duas opções \n)");
                             numeroDeOperacoes +=1;
                         }
                 }
-                //Aqui deverá vir a definição da estrutura de dados
+                //Definição da estrutura de dados
                 if(tipoPilha){
                     System.out.println("É definitivamente uma pilha!");
                 } else if (tipoFila){
                     System.out.println("É definitivamente uma fila!");
                     
+                }else if(!tipoPilha && tipoFilaDePrioridade ){
+                    System.out.println("Fila de priridade");
+                }else{
+                    System.out.println("Impossivel definir");
                 }
                 
-                System.out.println("Programa Encerrado!!!");
                 lista.clear();
+                System.out.println("Programa Encerrado!!!");
                 
             }else {
                 System.out.println("Digite um número inteiro entre 1 e 100! \n");
--- src/Pt_02/Teste.java
+++ src/Pt_02/Teste.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Pt_02;
+import java.util.Scanner;
 import Metodos_Ordenacao.Metodos;
 import java.util.concurrent.TimeUnit;
 
@@ -12,37 +13,102 @@ import java.util.concurrent.TimeUnit;
  */
 public class Teste {
     public static void main (String[] args){
-        int []vet = new int [1000000];
         
+        Scanner sc = new Scanner(System.in);
+        
+        
+        for(int j=0;j<5;j++){
+         
+        System.out.println("Digite o tamanho do Array a ser ordenado:");
+        int tamanhoDoVetor = sc.nextInt();
+        int vet[] = new int[tamanhoDoVetor];
+        
+        
+        //preenche o tamanho do vetor aleatóriamente
         for (int i=0; i < vet.length; i++){
             vet[i] = (int) (Math.random() * vet.length);
         }
         
-        // Desordenado
-        //System.out.println("Vetor desornedado\n");
+//         Desordenado
+//        System.out.println("Vetor desornedado\n");
 //        for(int i=0;i < vet.length; i++){
 //            System.out.println(vet[i] + " ");   
-//        }
+//       }
         
         
+        if(vet.length < 1000000){
+            //BubbleSort
+            long tempoInicialB = System.currentTimeMillis();
+            Metodos.bubbleSort(vet);
+            long tempoFinalB = System.currentTimeMillis();
+
+            long tempoDexecussaoB = tempoFinalB - tempoInicialB  ;
+
+            System.out.println("Tempo de execução BubbleSort: "+tempoDexecussaoB);
+            
+            //InsertionSort
+            long tempoInicialIns = System.currentTimeMillis();
+            Metodos.isertionSort(vet);
+            long tempoFinalIns = System.currentTimeMillis();
+
+            long tempoDexecussaoIns = tempoFinalIns - tempoInicialIns  ;
+
+            System.out.println("Tempo de execução InsertionSort: "+tempoDexecussaoIns);
+            
+            //Selection
+            long tempoInicialSel = System.currentTimeMillis();
+            Metodos.selectionSort(vet);
+            long tempoFinalSel = System.currentTimeMillis();
+
+            long tempoDexecussaoSel = tempoFinalSel - tempoInicialSel  ;
+
+            System.out.println("Tempo de execução SelectionSort: "+tempoDexecussaoSel);
+            
+        }
         
-        
-        
+             
         //Ordenado
         //System.out.println("\n -------- Vetor ordenado-------\n");
         //Captura o tempo inicial
-        long tempoInicial = System.currentTimeMillis();
+        
+        //ShellSort
+        long tempoInicialShel = System.currentTimeMillis();
+        Metodos.shellSort(vet);
+        //Captura o tempo final
+        long tempoFinalShel = System.currentTimeMillis();
+        //Diferença
+        long tempoDeExecucaoShel = (tempoFinalShel - tempoInicialShel);
+        
+        System.out.println("Tempo de execução ShellSort: "+tempoDeExecucaoShel);
+        
+        //QuickSort
+        long tempoInicialQuick = System.currentTimeMillis();
+        Metodos.quickSort(vet);
+        //Captura o tempo final
+        long tempoFinalQuick = System.currentTimeMillis();
+        //Diferença
+        long tempoDeExecucaoQuick = (tempoFinalQuick - tempoInicialQuick);
+        
+        System.out.println("Tempo de execução QuickSort: "+tempoDeExecucaoQuick);
+        
+       //MergeSort
+        long tempoInicialMerg = System.currentTimeMillis();
         Metodos.mergeSort(vet);
         //Captura o tempo final
-        long tempoFinal = System.currentTimeMillis();
+        long tempoFinalMerg = System.currentTimeMillis();
         //Diferença
-        long tempoDeExecucao = (tempoFinal - tempoInicial);
+        long tempoDeExecucaoMerg = (tempoFinalMerg - tempoInicialMerg);
+        
+        System.out.println("Tempo de execução ShellSort: "+tempoDeExecucaoMerg);
+            
+        
         
 //        for(int i=0;i < vet.length; i++){
 //            System.out.println(vet[i] + "");   
 //        }
         
-        System.out.println("Tempo de Execução: " + tempoDeExecucao);
+
+    }
         
 }
  
